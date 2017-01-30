2e5cb7a0613f93dff1ca8e1c2c241a628d439ba3
alashoor <mahyms.com@gmail.com>
2017-01-30 23:39:54 +0300

    StepOne

---
+++ IT/IT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IT.Startup))]
+namespace IT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
